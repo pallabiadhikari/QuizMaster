b700709267be25071e0f88c97c02e9b2b9d9b8ea
Pallabi-62 <pallabiadhikari62@gmail.com>
2026-01-06 15:07:45 +0545

    Add migrations

--- quiz_platform/settings.py
+++ quiz_platform/settings.py
@@ -24,7 +24,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-s%p$13s*o5d%4hkyhqhd3#r)t%0bs!vkvsgf93wv&s_qvv@(xw'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
 ALLOWED_HOSTS = [
     "django-m232.onrender.com",
